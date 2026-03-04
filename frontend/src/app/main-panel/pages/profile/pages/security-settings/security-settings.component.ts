3d5299a68721859dfc62e470d5863f3ff42493e3
Roosevelt Franklin <roosevelt.franklin@chatguru.com.br>
2026-03-04 20:28:12 -0300

    exercicio aula 01 finalizado

---
+++ frontend/src/app/main-panel/pages/profile/pages/security-settings/security-settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-settings',
+  imports: [],
+  templateUrl: './security-settings.component.html',
+  styleUrl: './security-settings.component.css'
+})
+export class SecuritySettingsComponent {
+
+}
---
+++ frontend/src/app/main-panel/pages/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+
+}
