e5a6c41bf7989f08d15240e14c9dbabc7a3df34d
Roosevelt Franklin <roosevelt.franklin@chatguru.com.br>
2026-03-10 17:46:45 -0300

    exercicio aula 02 finalizado

---
+++ frontend/src/app/main-panel/pages/loan/components/loan-simulator/loan-simulator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-simulator',
+  imports: [],
+  templateUrl: './loan-simulator.component.html',
+  styleUrl: './loan-simulator.component.css'
+})
+export class LoanSimulatorComponent {
+
+}
